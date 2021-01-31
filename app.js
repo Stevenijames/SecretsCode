16314c49d38d1ed2aa1efdd536512f73d4307a19
Steven Ijames <ijamesproductions1@gmail.com>
2021-01-31 12:54:16 -0700

    use of md5

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 const app = express();
 
 //console logging our "secret" key information
-console.log(process.env.SECRET);
+console.log(md5("12345678"));
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -29,7 +29,6 @@ const userSchema = new mongoose.Schema({
 
 //Encrypt Only the "password" field
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -49,7 +48,7 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     //only registering the "secrets" page from register and login 
@@ -64,7 +63,7 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({ email: username }, function (err, foundUser) {
         if (err) {
